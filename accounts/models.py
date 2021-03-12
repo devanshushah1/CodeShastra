0bb0991e499cceaacde4fe65aeb645a9b9be4436
Dhruvi <dhruvi.jodhawat@gmail.com>
2021-03-12 15:00:36 +0530

    models added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,90 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+
+    def create_type1(self, email, password, **extra_fields):
+        """
+        Create and save a type1 with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        # extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_type1', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Type1 must have is_staff=True.')
+        # if extra_fields.get('is_superuser') is not True:
+        #     raise ValueError('Superuser must have is_superuser=True.')
+        # return self.create_user(email, password, **extra_fields)
+
+    def create_type2(self, email, password, **extra_fields):
+        """
+        Create and save a type1 with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        # extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_type2', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Type2 must have is_staff=True.')
+        # if extra_fields.get('is_superuser') is not True:
+        #     raise ValueError('Superuser must have is_superuser=True.')
+        # return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(max_length=255, unique=True)
+    is_type1 = models.BooleanField(default=False)
+    is_type2 = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+# firstname
+# email
+# pass
+# pass2
+# phone number
